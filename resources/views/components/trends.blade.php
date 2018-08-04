7d63d3887f03c2d0baceed9557e22b00ea1b5e6b
Christopher Landon <chris@automashion.com>
2018-08-03 22:09:23 -0700

    Remove unused Trending Hashtag placeholder

--- resources/views/components/trends.blade.php
+++ resources/views/components/trends.blade.php
@@ -2,11 +2,6 @@
     <div class="bg-white p-3 mb-3">
         <h5>United States trends</h5>
         <ul class="list-unstyled mb-0">
-            <li class="mb-2">
-                <a href="#">#Laravel</a>
-                <br>
-                <span class="text-muted" style="font-size: 13px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est.</span>
-            </li>
             @foreach ($trendingHashtags as $key => $value)
                 <li class="mb-2">
                     <a href="{{ route('hashtags.index', strtolower($key)) }}">#{{ $key }}</a>
